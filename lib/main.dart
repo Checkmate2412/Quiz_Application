d22920ff84af40ee2be0057c162bf8273d8344a2
Eduardo Comploma Jr <eduardocomplomajr@gmail.com>
2023-02-16 11:40:49 +0800

    47. final vs const

--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,7 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-    var questions = [
+    const questions = [
       {
         'questionText': 'What\'s your favorite color?',
         'answers': ['Black', 'Red', 'Green', 'White'],
@@ -43,6 +43,12 @@ class _MyAppState extends State<MyApp> {
         'answers': ['Max', 'Max', 'Max', 'Max'],
       },
     ];
+
+    // var dummy = ['Hello'];
+    // dummy.add('Max');
+    // print(dummy);
+    // dummy = [];
+    // questions = []; // does not work if questions is a const
 
     return MaterialApp(
       home: Scaffold(
