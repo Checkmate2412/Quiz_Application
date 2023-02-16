5510f2e20005742e0e8a4d4745916bdaf4c11754
Eduardo Comploma Jr <eduardocomplomajr@gmail.com>
2023-02-16 13:11:20 +0800

    53. Calculating a Total Score

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -5,7 +5,7 @@ import 'question.dart';
 
 class Quiz extends StatelessWidget {
   final List<Map<String, Object>> questions;
-  final VoidCallback answerQuestion;
+  final Function answerQuestion;
   final int questionIndex;
 
   Quiz({
@@ -21,9 +21,11 @@ class Quiz extends StatelessWidget {
         Question(
           questions[questionIndex]['questionText'] as String,
         ),
-        ...(questions[questionIndex]['answers'] as List<String>).map(
+        ...(questions[questionIndex]['answers'] as List<Map<String, Object>>)
+            .map(
           (answer) {
-            return Answer(answerQuestion, answer);
+            return Answer(() => answerQuestion(answer['score']),
+                answer['text'] as String);
           },
         ).toList(),
       ],
